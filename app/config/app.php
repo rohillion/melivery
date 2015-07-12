44da1cb1e9d1d107fa5d234be9e12f40328b4893
rohillion <rohillion@hotmail.com>
2015-07-12 23:18:58 +0100

    fix session

--- app/config/app.php
+++ app/config/app.php
@@ -23,7 +23,7 @@ return array(
       | your application so that it is used when running Artisan tasks.
       |
      */
-    'url' => 'http://melivery.dev',
+    'url' => 'http://melivery.com',
     /*
       |--------------------------------------------------------------------------
       | Application Timezone
